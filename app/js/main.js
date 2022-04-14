d0f8a0db892092d5b104d91ea26fa2f922f4f18e
Lada-Novak <lada4445@yandex.ru>
2022-04-14 19:06:28 +0200

    start project

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+  $('.slider-top__items').slick({
+    prevArrow: '<button type="button" class="slider-top__prev"><img src="images/arrow__left.svg" alt="arrow_left"></button>',
+    nextArrow: '<button type="button" class="slider-top__next"><img src="images/arrow__right.svg" alt="arrow_right"></button>',
+  });
+});
