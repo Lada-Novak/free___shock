010ed2a2249317b425d7d06794fa82169aaebbaf
Lada-Novak <lada4445@yandex.ru>
2022-04-27 00:33:49 +0200

    started creatу section sales

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,8 @@ $(function(){
   $('.slider-top__items').slick({
     prevArrow: '<button type="button" class="slider-top__prev"><img src="images/icons/arrow__left.svg" alt="arrow_left"></button>',
     nextArrow: '<button type="button" class="slider-top__next"><img src="images/icons/arrow__right.svg" alt="arrow_right"></button>',
+    autoplay: true,
+    autoplaySpead: 3000,
   });
 
   var mixer = mixitup('.article');
